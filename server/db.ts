1cb1cd6e6fb120f8db17880587e8ac7c864ea0f5
Noman Younus <nomilogic@gmail.com>
2025-07-29 18:35:31 +0000

    company profile added

--- server/db.ts
+++ server/db.ts
@@ -1,20 +1,21 @@
-import { Pool } from 'pg';
-import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from "pg";
+import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "@shared/schema";
 
 if (!process.env.DATABASE_URL) {
+  console.log("DATABASE_URL", process.env.DATABASE_URL);
   throw new Error(
     "DATABASE_URL must be set. Did you forget to provision a database?",
   );
 }
 
-export const pool = new Pool({ 
+export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false
+    rejectUnauthorized: false,
   },
   max: 20,
   connectionTimeoutMillis: 30000,
-  idleTimeoutMillis: 30000
+  idleTimeoutMillis: 30000,
 });
 export const db = drizzle({ client: pool, schema });
